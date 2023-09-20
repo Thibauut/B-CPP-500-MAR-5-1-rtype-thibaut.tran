1da904ee83bdbf0198975e3abae1b01a6ee94d39
yanis denat <yanis.denat@epitech.eu>
2023-09-20 02:46:52 +0200

    callback after connection

--- server/Socket/TCP/TCPConnexion.hpp
+++ server/Socket/TCP/TCPConnexion.hpp
@@ -7,6 +7,7 @@
 
 #pragma once
 #include <boost/asio.hpp>
+#include <boost/bind/bind.hpp>
 #include <iostream>
 
 using namespace std;
@@ -14,17 +15,34 @@ using namespace std;
 class TCPConnexion : public enable_shared_from_this<TCPConnexion> {
     public:
         typedef boost::shared_ptr<TCPConnexion> pointer;
+
         TCPConnexion(boost::asio::io_service& ioService) : _socket(ioService){}
+
         static pointer create_new_connection(boost::asio::io_service& service) {
             return pointer(new TCPConnexion(service));
         }
         boost::asio::ip::tcp::socket& socket() { return _socket;};
 
         void start() {
+            _message = "Salut pd";
+            boost::asio::async_write(_socket, boost::asio::buffer(_message),
+                boost::bind(&TCPConnexion::handle_write, this,
+                    boost::asio::placeholders::error
+            ));
         };
 
         ~TCPConnexion() {};
+
     private:
+        void handle_write(const boost::system::error_code& error) {
+            if (!error) {
+                cout << "Message sent" << endl;
+            } else {
+                cerr << "Error: " << error.message() << endl;
+                _socket.close();
+            }
+        };
+
         std::string _message;
         boost::asio::ip::tcp::socket _socket;
 };
