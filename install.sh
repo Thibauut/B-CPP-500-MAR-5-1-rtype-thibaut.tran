9202bf9842b2f473b3a77a4e8070f4afcb3cceac
Thibauut <91698852+Thibauut@users.noreply.github.com>
2023-09-21 22:06:29 +0200

    Delete install.sh

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cmake -S . -B ./build/ && make -C ./build/
-
-# cmake -B . -S . -DCMAKE_TOOLCHAIN_FILE=vcpkg/scripts/buildsystems/vcpkg.cmake
-
-# git submodule add https://github.com/SFML/SFML.git libs/SFML
