5691c85926cbe5c17df6c7ce2ca7b05c77233a7d
Thibaut <Thibauttran13@gmail.com>
2023-09-21 20:46:23 +0200

    ADD client/graphical menu

--- client/graphical/utils/sfml_func.cpp
+++ client/graphical/utils/sfml_func.cpp
@@ -9,14 +9,60 @@
 
 SfmlFunc::SfmlFunc() = default;
 
-sf::Sprite SfmlFunc::CreateSprite(std::string path, float posX, float posY, float scaleX, float scaleY)
-{
-    sf::Texture texture;
-    sf::Sprite sprite;
+void SfmlFunc::LoadSprite(sf::Sprite& sprite,  sf::Texture &texture, const std::string& imagePath, float posX, float posY, float scaleX, float scaleY) {
+    if (texture.loadFromFile(imagePath)) {
+        sprite.setTexture(texture);
+        sprite.setPosition(posX, posY);
+        sprite.setScale(scaleX, scaleY);
+    } else {
+        std::cout << "Error: could not load image: " << imagePath << std::endl;
+    }
+}
 
-    texture.loadFromFile(path);
-    sprite.setTexture(texture);
-    sprite.setPosition(posX, posY);
-    sprite.setScale(scaleX, scaleY);
-    return sprite;
+void SfmlFunc::CreateTextField(sf::RectangleShape& textField, sf::Text& text, const sf::Font& font, const std::string& labelText, const sf::Vector2f& size, const sf::Vector2f& position) {
+    textField.setSize(size);
+    textField.setFillColor(sf::Color::Black);
+    textField.setOutlineColor(sf::Color::White);
+    textField.setOutlineThickness(5.0f);
+    textField.setPosition(position);
+
+    text.setFont(font);
+    text.setString(labelText);
+    text.setCharacterSize(24);
+    text.setFillColor(sf::Color::White);
+    text.setPosition(position - sf::Vector2f(0, 40));
+}
+
+void SfmlFunc::createText(sf::Text& text, const sf::Font& font, const std::string& content, unsigned int characterSize, const sf::Color& fillColor, const sf::Vector2f& position) {
+    text.setFont(font);
+    text.setString(content);
+    text.setCharacterSize(characterSize);
+    text.setFillColor(fillColor);
+    text.setPosition(position);
+}
+
+void SfmlFunc::CreateButton(sf::RectangleShape& button, sf::Text& buttonText, const sf::Font& font, const std::string& label, const sf::Vector2f& size, const sf::Vector2f& position, int moveText) {
+    button.setSize(size);
+    button.setFillColor(sf::Color::Black);
+    button.setOutlineColor(sf::Color::White);
+    button.setOutlineThickness(5.0f);
+    button.setPosition(position);
+
+    buttonText.setFont(font);
+    buttonText.setString(label);
+    buttonText.setCharacterSize(24);
+    buttonText.setFillColor(sf::Color::White);
+    buttonText.setPosition(position + sf::Vector2f(moveText, 10));
+}
+
+void SfmlFunc::HandleButtonHover(sf::RectangleShape& button, sf::Text& buttonText, bool& isButtonHovered, const sf::Vector2f& mousePos, const sf::FloatRect& buttonBounds) {
+    if (buttonBounds.contains(mousePos)) {
+        isButtonHovered = true;
+        button.setScale(1.01f, 1.01f);
+        buttonText.setScale(1.01f, 1.01f);
+    } else {
+        isButtonHovered = false;
+        button.setScale(1.0f, 1.0f);
+        buttonText.setScale(1.0f, 1.0f);
+    }
 }
