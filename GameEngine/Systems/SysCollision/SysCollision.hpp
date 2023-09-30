9c1b9e183a826c67c981f1e37f9e12dba9c3de39
unknown <raphael.de-monchy@epitech.eu>
2023-09-30 16:20:33 +0200

    [ADD] SysCollision.hpp

--- GameEngine/Systems/SysCollision/SysCollision.hpp
+++ GameEngine/Systems/SysCollision/SysCollision.hpp
@@ -6,19 +6,20 @@
 */
 
 #pragma once
-#include "ASystem.hpp"
+#include "../ASystem/ASystem.hpp"
+#include "../../Utils/Utils.hpp"
 
 namespace GameEngine {
 
     class SysCollision : public ISystem {
         public:
-            SysCollision() {};
-            ~SysCollision() {};
+            SysCollision(std::shared_ptr<EntityManager> entityManagerPtr) : _entityManager(entityManagerPtr) {};
+            virtual ~SysCollision() = default;
 
-            virtual void run() override {
+            virtual void update() {
                 // Logique de détection de collision/HITBOX (à implémenter)
             }
         private:
+            std::shared_ptr<EntityManager> _entityManager;
     };
-
 }
