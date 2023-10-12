a1f473d347170d97e9326aaa0f9ea8979fe94e16
Mohamed Mansour <mohamed.mansour@epitech.eu>
2023-10-12 02:33:02 +0200

    [Add] sysAI

--- GameEngine/Systems/SysAI/SysAI.hpp
+++ GameEngine/Systems/SysAI/SysAI.hpp
@@ -7,6 +7,8 @@
 
 #pragma once
 #include "../ASystem/ASystem.hpp"
+#include "../../Components/Position/Position.hpp"
+#include <chrono>
 
 namespace GameEngine {
     template <class T>
@@ -23,24 +25,32 @@ namespace GameEngine {
                     return;
                 }
 
-                for (const auto& entityPtr : EntityManager_) {
-                    auto aiComponent = entityPtr->getComponent<AI>();
+                for (std::shared_ptr<Entity>& entityPtr : EntityManager_) {
+                    auto aiComponent = entityPtr->getComponentByType<AI>(CONFIG::CompType::AI);
 
                     if (!aiComponent) {
                         std::cout << "No AI component found on this entity." << '\n';
-                    } else if (aiComponent == 1) {
-
+                    } else if (aiComponent->getAiType() == CONFIG::AiType::MOB1) {
+                        mobalgo1();
                     }
-                //  if fonction mob
-                // if fonction player
-                }
-
-
-            }
+                    }
+                };
 
         private:
+            bool mobalgo1() {
+                position_ = entityPtr->getComponentByType<Position>(CONFIG::CompType::POSITION);
+                position_->setPosition(position_->getPositionX() - 1, position_->setPositionY());
+                std::chrono::milliseconds couldown = aiComponent->getCouldown();
+                std::time_t current_time = std::chrono::system_clock::to_time_t(now);
+                // waiting the couldown time thanks current_time value
+                std::time_t limit_time = current_time + couldown;
+                while (current_time < limit_time) {
+                    current_time = std::chrono::system_clock::to_time_t(now);
+                }
+            };
             bool isRunning;
             std::list<std::shared_ptr<Entity>> &_entities;
+            std::shared_ptr<Position> &position_;
     };
 
 }
