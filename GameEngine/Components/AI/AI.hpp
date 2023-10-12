2a70e15d51deb0717493807052f4878a207a5ba3
Mohamed Mansour <mohamed.mansour@epitech.eu>
2023-10-12 02:34:40 +0200

    [add] aiComponent

--- GameEngine/Components/AI/AI.hpp
+++ GameEngine/Components/AI/AI.hpp
@@ -7,12 +7,13 @@
 
 #pragma once
 #include "../AComponent/AComponent.hpp"
+#include <chrono>
 
 namespace GameEngine {
 
     class AI : public IComponent {
         public:
-            AI(CONFIG::CompType type, int id) : _idComponent(id), _type(type), _activate(false) {}
+            AI(CONFIG::CompType type, CONFIG::AiType aiType, int id, std::chrono::milliseconds couldown) : _idComponent(id), _type(type), _aiType(aiType), _couldown(couldown), _activate(false) {}
             ~AI() = default;
 
             void setAIActive(bool activate) {
@@ -21,24 +22,23 @@ namespace GameEngine {
             }
 
             bool isAIactive() const {
-                std::cout << "AI getter : state =" << _activate << std::endl;
+                std::cout << "AI getter : state = " << _activate << std::endl;
                 return _activate;
-            }
-            template<class Archive>
-            void serialize(Archive & ar, const unsigned int version) {
-                ar & _idComponent;
-                ar & _type;
-                ar & _activate;
             }
 
             virtual CONFIG::CompType getType() {return _type;};
             virtual void setType(const CONFIG::CompType type) {_type = type;};
             virtual int getId() {return _idComponent;};
             virtual void setId(const int id) {_idComponent = id;};
+            CONFIG::AiType getAiType() {return _aiType;};
+            void setAiType(const CONFIG::AiType aiType) {_aiType = aiType;};
+            std::chrono::milliseconds getCouldown() {return _couldown;};
 
         protected:
             int _idComponent;
             CONFIG::CompType _type;
+            CONFIG::AiType _aiType;
+            std::chrono::milliseconds _couldown;
 
         private:
             bool _activate;
