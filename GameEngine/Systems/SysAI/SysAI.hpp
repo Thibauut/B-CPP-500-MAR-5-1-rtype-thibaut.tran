1d11895730c73266b28a3ad8c22eabc90edf9e99
Mohamed Mansour <mohamed.mansour@epitech.eu>
2023-10-19 22:59:08 +0200

    [fix] AI lvl 2 + [mod] need modifie timeout

--- GameEngine/Systems/SysAI/SysAI.hpp
+++ GameEngine/Systems/SysAI/SysAI.hpp
@@ -9,6 +9,7 @@
 #include "../ASystem/ASystem.hpp"
 #include "../../Components/Position/Position.hpp"
 #include "../../Components/AI/AI.hpp"
+#include "../../Components/Weapon/Weapon.hpp"
 #include <chrono>
 #include "../../Utils/Timeout.hpp"
 
@@ -16,8 +17,10 @@ namespace GameEngine {
     class SysAI : public ISystem {
         public:
             SysAI(std::shared_ptr<EntityManager> entityList) : _entities(entityList), _sinusoidalY(false), _stateMobOneY(false), _stateMobOneX(false), isRunning(true) {
-                canMoveX = Timeout(0.01);
-                canMoveY = Timeout(0.001);
+                canMoveXMob1 = Timeout(0.01);
+                canMoveYMob1 = Timeout(0.001);
+                canMoveXMob2 = Timeout(0.001);
+                canMoveYMob2 = Timeout(0.001);
             }
             ~SysAI() {};
             bool isAI() { return true; };
@@ -36,15 +39,30 @@ namespace GameEngine {
                         if (aiComponent->getAiType() == CONFIG::AiType::MOB2) {
                             mobalgo2(entityPtr);
                         }
+                        if (aiComponent->getAiType() == CONFIG::AiType::BOSS) {
+                            mobAlgoBoss(entityPtr);
+                        }
                     }
                 }
         };
+
+        bool checkClostestToAi(int posxP1, int posxP2, int posxAi) {
+            int value1 = posxP1 - posxAi;
+            int value2 = posxP2 - posxAi;
+            if (value1 < 0)
+                value1 = value1 * -1;
+            if (value2 < 0)
+                value2 = value2 * -1;
+            if (value1 < value2)
+                return true;
+            return false;
+        }
 
         private:
             void mobalgo1(std::shared_ptr<Entity> entity) {
                 auto aiComponent = entity->getComponentByType<AI>(CONFIG::CompType::AI);
                 auto posComponent = entity->getComponentByType<Position>(CONFIG::CompType::POSITION);
-                if (canMoveX.can_execute()) {
+                if (canMoveXMob1.can_execute()) {
                     // posComponent->setPosition(posComponent->getPositionX() - 1, posComponent->getPositionY());
                     if (posComponent->getPositionX() <= 1100)
                         _stateMobOneX = true;
@@ -54,9 +72,9 @@ namespace GameEngine {
                         posComponent->setPosition(posComponent->getPositionX() - 1, posComponent->getPositionY());
                     else
                         posComponent->setPosition(posComponent->getPositionX() + 1, posComponent->getPositionY());
-                    canMoveX.Start();
+                    canMoveXMob1.Start();
                 }
-                if (canMoveY.can_execute()) {
+                if (canMoveYMob1.can_execute()) {
                     if (posComponent->getPositionY() >= 980)
                     _stateMobOneY = true;
                     else if (posComponent->getPositionY() <= 5)
@@ -65,19 +83,119 @@ namespace GameEngine {
                         posComponent->setPosition(posComponent->getPositionX(), posComponent->getPositionY() + 1);
                     else
                         posComponent->setPosition(posComponent->getPositionX(), posComponent->getPositionY() - 1);
-                    canMoveY.Start();
+                    canMoveYMob1.Start();
                 }
             };
             void mobalgo2(std::shared_ptr<Entity> entity) {
                 auto aiComponent = entity->getComponentByType<AI>(CONFIG::CompType::AI);
                 auto posComponent = entity->getComponentByType<Position>(CONFIG::CompType::POSITION);
+                auto weapComponent = entity->getComponentByType<Weapon>(CONFIG::CompType::WEAPON);
+                std::list<std::shared_ptr<Entity>> playerList = _entities.get()->getEntitiesByType(1);
+                if (playerList.size() == 1) {
 
+                    _clostestPY = playerList.front().get()->getComponentByType<Position>(CONFIG::CompType::POSITION).get()->getPositionY();
+                    _clostestPX = playerList.front().get()->getComponentByType<Position>(CONFIG::CompType::POSITION).get()->getPositionX();
+                    std::cout << "Clostest player Position Y --> " << _clostestPY << std::endl;
+                    std::cout << "Clostest player Position X --> " << _clostestPX << std::endl;
+                } else {
+
+                    for (std::shared_ptr<Entity> &entity : playerList) {
+
+                        // std::cout << "Player num : " << entity.get()->getId() << std::endl;
+                        // std::cout << "Y = " << entity.get()->getComponentByType<Position>(CONFIG::CompType::POSITION).get()->getPositionY() << std::endl;
+                        // std::cout << "X = " << entity.get()->getComponentByType<Position>(CONFIG::CompType::POSITION).get()->getPositionX() << std::endl;
+                    }
+                }
+                if (_clostestPY > posComponent->getPositionY() + 150) {
+                    // weapComponent->setShooting(false, 0);
+                    if (weapComponent->canShoot()) {
+                        weapComponent->setShooting(true, 0);
+                    }
+                } else if (_clostestPY < posComponent->getPositionY() - 150) {
+                    // weapComponent->setShooting(false, 0);
+                    if (weapComponent->canShoot()) {
+                        weapComponent->setShooting(true, 0);
+                    }
+                } else if (_clostestPY == posComponent->getPositionY()) {
+                    std::cout << "Shooting !" << std::endl;
+                }
+                if (_clostestPY > posComponent->getPositionY()) {
+                    if (canMoveYMob2.can_execute()) {
+                        posComponent->setPosition(posComponent->getPositionX(), posComponent->getPositionY() + 1);
+                        canMoveYMob2.Start();
+                    }
+                } else if (_clostestPY < posComponent->getPositionY()) {
+                    if (canMoveYMob2.can_execute()) {
+                        posComponent->setPosition(posComponent->getPositionX(), posComponent->getPositionY() - 1);
+                        canMoveYMob2.Start();
+                    }
+                }
+                if (_clostestPX > posComponent->getPositionX() + 250) {
+                    if (canMoveXMob2.can_execute()) {
+                        posComponent->setPosition(posComponent->getPositionX() + 1, posComponent->getPositionY());
+                        canMoveXMob2.Start();
+                    }
+                } else if (_clostestPX < posComponent->getPositionX() - 250) {
+                    if (canMoveXMob2.can_execute()) {
+                        posComponent->setPosition(posComponent->getPositionX() - 1, posComponent->getPositionY());
+                        canMoveXMob2.Start();
+                    }
+                }
+                if (_clostestPX < posComponent->getPositionX() + 250 && _clostestPX >= posComponent->getPositionX()) {
+                    if (canMoveXMob2.can_execute() && posComponent->getPositionX() > 100) {
+                        posComponent->setPosition(posComponent->getPositionX() - 1, posComponent->getPositionY());
+                        canMoveXMob2.Start();
+                    }
+                } else if (_clostestPX > posComponent->getPositionX() - 250 && _clostestPX <= posComponent->getPositionX()) {
+                    if (canMoveXMob2.can_execute() && posComponent->getPositionX() < 1820) {
+                        posComponent->setPosition(posComponent->getPositionX() + 1, posComponent->getPositionY());
+                        canMoveXMob2.Start();
+                    }
+                }
+                // if (_clostestPY < posComponent->getPositionY() + 150 && _clostestPY >= posComponent->getPositionY()) {
+                //     if (canMoveYMob2.can_execute() && posComponent->getPositionY() > 100) {
+                //         posComponent->setPosition(posComponent->getPositionX(), posComponent->getPositionY() - 1);
+                //         canMoveYMob2.Start();
+                //     }
+                // } else if (_clostestPY > posComponent->getPositionY() - 150 && _clostestPY <= posComponent->getPositionY()) {
+                //     if (canMoveYMob2.can_execute() && posComponent->getPositionY() > 980) {
+                //         posComponent->setPosition(posComponent->getPositionX(), posComponent->getPositionY() + 1);
+                //         canMoveYMob2.Start();
+                //     }
+                // }
+                std::cout << "Mob X --> " << posComponent->getPositionX() << std::endl;
+                std::cout << "Mob Y --> " << posComponent->getPositionY() << std::endl;
             };
-            Timeout canMoveX;
-            Timeout canMoveY;
+            void mobAlgoBoss(std::shared_ptr<Entity> entity) {
+                auto aiComponent = entity->getComponentByType<AI>(CONFIG::CompType::AI);
+                auto posComponent = entity->getComponentByType<Position>(CONFIG::CompType::POSITION);
+                auto weapComponent = entity->getComponentByType<Weapon>(CONFIG::CompType::WEAPON);
+                auto weapBossComponent = entity->getComponentByType<Weapon>(CONFIG::CompType::WEAPON);
+                std::list<std::shared_ptr<Entity>> playerList = _entities.get()->getEntitiesByType(1);
+                for (std::shared_ptr<Entity> &entity : playerList) {
+                    if (entity->getComponentByType<Position>(CONFIG::CompType::POSITION).get()->getPositionY() <= posComponent->getPositionY() + 150 && entity->getComponentByType<Position>(CONFIG::CompType::POSITION).get()->getPositionY() >= posComponent->getPositionY() - 150) {
+                        std::cout << "In front shooting ! " << std::endl;
+                        if (weapBossComponent->canShoot()) {
+                            weapBossComponent->setShooting(true, 0);
+                        }
+                    } else {
+                        std::cout << "NOT In front shooting ! " << std::endl;
+                        if (weapComponent->canShoot()) {
+                            weapComponent->setShooting(true, 0);
+                        }
+                    }
+                }
+            };
+            Timeout canMoveXMob1;
+            Timeout canMoveYMob1;
+            Timeout canMoveXMob2;
+            Timeout canMoveYMob2;
             bool _sinusoidalY;
             int _basicPositionY;
             int _basicPositionX;
+            int _clostestPY;
+            int _clostestPX;
+            bool _cmpr;
             bool _stateMobOneY;
             bool _stateMobOneX;
             bool isRunning;
