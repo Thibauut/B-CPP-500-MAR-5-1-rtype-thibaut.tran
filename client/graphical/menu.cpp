dacdc46e0519547af34b56f6e6059be7909cb5cd
Thibaut <Thibauttran13@gmail.com>
2023-11-05 13:45:11 +0100

    [FIX] pong compatibility

--- client/graphical/menu.cpp
+++ client/graphical/menu.cpp
@@ -131,8 +131,10 @@ void Menu::Draw() {
         _window->draw(_button);
         _window->draw(_buttonText);
 
-        _window->draw(_buttonEditor);
-        _window->draw(_buttonEditorText);
+        if (_titleFirst == "R-TYPE") {
+            _window->draw(_buttonEditor);
+            _window->draw(_buttonEditorText);
+        }
     }
     if (_isConnected && !_selectedRoom && !_isCreatingRoom) {
         _window->draw(_roomMenu);
